6a89a2fd6bfff0819d2777ef1049de9ac2ec0dd4
Dave Pinchoff <dpinchoff@gmail.com>
2017-07-23 16:26:14 -0400

    4: Viewing Text Files

--- cms.rb
+++ cms.rb
@@ -3,16 +3,25 @@ require "sinatra/reloader"
 require "tilt/erubis"
 
 
-def get_data_file_names
-  results = []
-  path = Dir.pwd + "/data"
-  data_dir = Dir.new(path)
-  data_dir.each { |file| results << file if file.match?(/.+\..+/) }
+def data_dir
+  Dir.new(Dir.pwd + "/data")
+end
 
-  results
+def data_file_names
+  data_dir.select { |file| file.match?(/.+\..+/) }
 end
 
 get "/" do
-  @data_files = get_data_file_names
+  @data_files = data_file_names
+
   erb :index, layout: :layout
+end
+
+get "/:filename" do
+  @title = params[:filename]
+  path = Dir.pwd + "/data/" + params[:filename]
+  @file = File.new(path)
+  headers["Content-Type"] = "text/plain"
+  headers["Title"] = "#{@title}"
+  erb :file, layout: false
 end
