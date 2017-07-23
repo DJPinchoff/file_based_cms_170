6d26f38b384af8ebfb1c8089da8eebb13e19bd91
Dave Pinchoff <dpinchoff@gmail.com>
2017-07-23 15:33:14 -0400

    #3: Adding an index page

--- cms.rb
+++ cms.rb
@@ -2,6 +2,17 @@ require "sinatra"
 require "sinatra/reloader"
 require "tilt/erubis"
 
+
+def get_data_file_names
+  results = []
+  path = Dir.pwd + "/data"
+  data_dir = Dir.new(path)
+  data_dir.each { |file| results << file if file.match?(/.+\..+/) }
+
+  results
+end
+
 get "/" do
-  erb :home, layout: :layout
+  @data_files = get_data_file_names
+  erb :index, layout: :layout
 end
