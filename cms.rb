1d21f115c66929f510c0207a8fcd0d48c7ca9819
Dave Pinchoff <dpinchoff@gmail.com>
2017-11-18 16:34:49 -0500

    Final version

--- cms.rb
+++ cms.rb
@@ -1,8 +1,11 @@
 require "sinatra"
 require "sinatra/reloader"
 require "tilt/erubis"
+require "bcrypt"
 require "redcarpet"
 require "fileutils"
+require "yaml"
+require 'pry'
 
 configure do
   enable :sessions
@@ -48,6 +51,37 @@ def parse_content(path, pure_content=nil)
   end
 end
 
+def load_users_and_passwords
+  path = if ENV["RACK_ENV"] == "test"
+    File.expand_path("../test/users.yml", __FILE__)
+  else
+    File.expand_path("../users.yml", __FILE__)
+  end
+  YAML.load_file(path)
+end
+
+def valid_username_and_password?(username, password)
+  credentials = load_users_and_passwords
+  
+  if credentials.key?(username)
+    bcrypt_password = BCrypt::Password.new(credentials[username])
+    bcrypt_password == password
+  else
+    false
+  end
+end
+
+def signed_in?
+  !!session[:username]
+end
+
+def redirect_user_if_not_signed_in
+  if !signed_in?
+    session[:message] = "You must be signed in to do that."
+    redirect "/"
+  end
+end
+
 # List all files in the data directory
 get "/" do
   @filenames = data_filenames
@@ -69,6 +103,7 @@ end
 
 # Display form to edit contents of file
 get "/:filename/edit" do
+  redirect_user_if_not_signed_in
   @filename = params[:filename]
   if valid_file?(@filename)
     @path = File.join(data_path, @filename)
@@ -83,6 +118,7 @@ end
 
 # Update/save changes made to file
 post "/:filename/update" do
+  redirect_user_if_not_signed_in
   filename = params[:filename]
   content = params[:content]
   path = File.join(data_path, filename)
@@ -93,11 +129,13 @@ end
 
 # Create a new file
 get "/new_file/create" do
+  redirect_user_if_not_signed_in
   erb :create, layout: :layout
 end
 
 # Save a valid new file
 post "/new_file/save" do
+  redirect_user_if_not_signed_in
   if params[:file]
     filename = params[:file]
     if valid_filename?(filename)
@@ -114,6 +152,7 @@ post "/new_file/save" do
 end
 
 post "/:filename/delete" do
+  redirect_user_if_not_signed_in
   filename = params[:filename]
   if valid_filename?(filename)
     File.delete(File.join(data_path, filename))
@@ -123,4 +162,26 @@ post "/:filename/delete" do
     session[:message] = "#{filename} does not exist! Try one of these files:"
     redirect "/"
   end
+end
+
+get "/users/signin" do
+  erb :signin, layout: :layout
+end
+
+post "/users/authenticate" do
+  if valid_username_and_password?(params[:username], params[:password])
+    session[:username] = params[:username]
+    session[:message] = "Welcome!"
+    redirect "/"
+  else
+    session[:message] = "Invalid credentials"
+    status 422
+    erb :signin, layout: :layout
+  end
+end
+
+post "/users/signout" do
+  session[:message] = "You have been signed out."
+  session.delete(:username)
+  redirect "/"
 end
--- test/cms_test.rb
+++ test/cms_test.rb
@@ -3,6 +3,7 @@ ENV["RACK_ENV"] = "test"
 require "minitest/autorun"
 require "rack/test"
 require "fileutils"
+require "yaml"
 
 require_relative "../cms"
 
@@ -21,10 +22,18 @@ class AppTest < Minitest::Test
     FileUtils.rm_rf(data_path)
   end
 
+  def session
+    last_request.env["rack.session"]
+  end
+
   def create_document(name, content = "")
     File.open(File.join(data_path, name), "w") do |file|
       file.write(content)
     end
+  end
+
+  def admin_session
+    { "rack.session" => { username: "admin" } }
   end
 
   def test_index
@@ -66,35 +75,44 @@ class AppTest < Minitest::Test
 
     get "/notafile.txt"
     assert_equal 302, last_response.status
-
-    get last_response["Location"]
-
-    assert_equal 200, last_response.status
-    assert_includes last_response.body, "notafile.txt does not exist!"
+    assert_equal "notafile.txt does not exist! Try one of these files:", session[:message]
   end
 
-  def test_editing_document
+  def test_editing_document_signed_in
     create_document "changes.txt", "changes"
-    get "/changes.txt/edit"
+    get "/changes.txt/edit", {}, admin_session
 
     assert_equal 200, last_response.status
     assert_includes last_response.body, "<textarea"
     assert_includes last_response.body, '<input type="submit"'
   end
 
-  def test_updating_document
-    create_document "changes.txt"
-    post "/changes.txt/update", content: "Changing"
+  def test_editing_document_signed_out
+    create_document "changes.txt", "changes"
+    get "/changes.txt/edit"
 
     assert_equal 302, last_response.status
+    assert_equal "You must be signed in to do that.", session[:message]
+  end
 
-    get last_response["Location"]
+  def test_updating_document_signed_in
+    create_document "changes.txt"
+    post "/changes.txt/update", { content: "Changing" }, admin_session
 
-    assert_includes last_response.body, "changes.txt has been updated successfully!"
+    assert_equal 302, last_response.status
+    assert_equal "changes.txt has been updated successfully!", session[:message]
 
     get "/changes.txt"
     assert_equal 200, last_response.status
     assert_includes last_response.body, "Changing"
+  end
+
+  def test_updating_document_signed_out
+    create_document "changes.txt"
+    post "/changes.txt/update", content: "Changing"
+
+    assert_equal 302, last_response.status
+    assert_equal "You must be signed in to do that.", session[:message]
   end
 
   def test_new_document_button_on_index_page
@@ -103,42 +121,97 @@ class AppTest < Minitest::Test
     assert_includes last_response.body, "New Document</a>"
   end
 
-  def test_view_new_document_form
-    get "/new_file/create"
+  def test_view_new_document_form_signed_in
+    get "/new_file/create", {}, admin_session
 
     assert_equal 200, last_response.status
     assert_includes last_response.body, "<input"
     assert_includes last_response.body, %q(<input type="submit")
   end
 
-  def test_create_new_document
-    post "/new_file/save", file: "test.txt"
-    assert_equal 302, last_response.status
+  def test_view_new_document_form_signed_out
+    get "/new_file/create"
 
-    get last_response["Location"]
-    assert_includes last_response.body, "test.txt was created"
+    assert_equal 302, last_response.status
+    assert_equal "You must be signed in to do that.", session[:message]
+  end
+
+  def test_create_new_document_signed_in
+    post "/new_file/save", { file: "test.txt" }, admin_session
+    assert_equal 302, last_response.status
+    assert_equal "test.txt was created.", session[:message]
 
     get "/"
     assert_includes last_response.body, "test.txt"
   end
 
+  def test_create_new_document_signed_out
+    post "/new_file/save", file: "test.txt"
+    assert_equal 302, last_response.status
+    assert_equal "You must be signed in to do that.", session[:message]
+  end
+
   def test_create_new_document_without_filename
-    post "/new_file/save", file: ""
+    post "/new_file/save", { file: "" }, admin_session
     assert_equal 422, last_response.status
     assert_includes last_response.body, "Invalid filename"
   end
 
-  def test_deleting_document
+  def test_deleting_document_signed_in
+    create_document("test.txt")
+
+    post "/test.txt/delete", {}, admin_session
+
+    assert_equal 302, last_response.status
+    assert_equal "test.txt was deleted.", session[:message]
+
+    get "/"
+    refute_includes last_response.body, "href=\"test.txt\""
+  end
+
+  def test_deleting_document_signed_out
     create_document("test.txt")
 
     post "/test.txt/delete"
 
     assert_equal 302, last_response.status
+    assert_equal "You must be signed in to do that.", session[:message]
+  end
+
+  def test_signin_form
+    get "/users/signin"
+
+    assert_equal 200, last_response.status
+    assert_includes last_response.body, "<input"
+    assert_includes last_response.body, %q(<input type="submit")
+  end
+
+  def test_signin
+    post "/users/authenticate", username: "admin", password: "secret"
+    assert_equal 302, last_response.status
+    assert_equal "Welcome!", session[:message]
+    assert_equal "admin", session[:username]
 
     get last_response["Location"]
-    assert_includes last_response.body, "test.txt was deleted."
+    assert_includes last_response.body, "Signed in as admin"
+  end
 
-    get "/"
-    refute_includes last_response.body, "test.txt"
+  def test_signin_with_bad_credentials
+    post "/users/authenticate", username: "guest", password: "shhhh"
+    assert_equal 422, last_response.status
+    assert_nil session[:username]
+    assert_includes last_response.body, "Invalid credentials"
+  end
+
+  def test_signout
+    get "/", {}, {"rack.session" => { username: "admin" } }
+    assert_includes last_response.body, "Signed in as admin"
+
+    post "/users/signout"
+    get last_response["Location"]
+
+    assert_nil session[:username]
+    assert_includes last_response.body, "You have been signed out"
+    assert_includes last_response.body, "Sign In"
   end
 end
